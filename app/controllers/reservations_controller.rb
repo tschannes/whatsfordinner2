03d5b8cf83ed31a141a47873e08d553b9a1fa243
dtredger <d.tredger@gmail.com>
2013-07-15 18:10:00 -0400

    ryan explains everything

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,7 +3,19 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    @reservation = @restaurant.reservations.find(params[:restaurant_id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reservation = Reservation.new(params[:reservation])
+
+    # Reservation.new( {:size => "400", :time => "9"} )
+    # don't do this -  params[:reservation][:restaurant_id]
+
+    flash[:notice] = @reservation
+    # @reservation = @restaurant.reservations.find(params[:restaurant_id])
+    if proper_reservation_size?(@reservation.size)
+          redirect_to user_path(current_user), :notice => "You successfully made a reservation!"
+        else
+          render "reservations/new", :notice => "Something went wrong!"
+    end
   end
 
   def new
@@ -22,5 +34,13 @@ class ReservationsController < ApplicationController
 
   def destroy
   end
+
+
+
+# is the reservation btw 2..20
+# number of seats available?
+# is the restaurant open
+# is the time slot available
+
 
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,5 +4,35 @@ class Reservation < ActiveRecord::Base
   belongs_to :user
   belongs_to :restaurant
 
+  validates :time, :presence => true
+  validate :proper_reservation_size
+
+  # probably need to define:
+  # how long do people eat for
+
+
+  # make sure that size is set to something
+  # make sure that restaurant time is between opening hours of restaurant - but where is this defined?
+
+  # eventually..
+  # make sure there are no conflicts
+    # make sure at all times that the reservation being created has enough seats remaining (for that entire time period)
+
+
+
+  def proper_reservation_size
+    if size && (size < 2 || size > 20)
+      errors.add(:size, "Party size must be between 2 and 20")
+    end
+  end
+
+  def number_of_seats_available
+  end
+
+  def restaurant_hours
+  end
+
+  def reservation_time_slot_open
+  end
 
 end
